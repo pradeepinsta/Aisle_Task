45e9422a51192b0b8f9c6208a677bc7c96684275
pradeepinsta <pradeepravi24@gmail.com>
2023-05-31 08:13:16 +0530

    Initial commit

---
+++ app/src/main/java/com/pradeepuct/aisletask/NotesScreen.kt
@@ -0,0 +1,11 @@
+package com.pradeepuct.aisletask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotesScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes_screen)
+    }
+}
